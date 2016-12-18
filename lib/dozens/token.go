a2f1a2b52cce3545999376be56da752ecb52c7f7
delphinus <delphinus@remora.cx>
2016-12-18 22:28:53 +0900

    Add logic to fetch & store config

--- lib/dozens/token.go
+++ lib/dozens/token.go
@@ -1,6 +1,127 @@
 package dozens
 
-// Token returns access token for dozens
-func Token() string {
-	return "hoge"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Songmu/prompter"
+	"github.com/pkg/errors"
+)
+
+// ConfigFile means config file for godo
+var ConfigFile = filepath.Join(os.Getenv("HOME"), ".config", "godo", "godo.json")
+
+// AuthInfo means info for authentication
+type AuthInfo struct {
+	Key  string `json:"key"`
+	User string `json:"user"`
+}
+
+// Configs stores config for godo
+type Configs struct {
+	AuthInfo
+	Token     string    `json:"token"`
+	IsValid   bool      `json:"isValid"`
+	ExpiredAt time.Time `json:"updatedAt"`
+}
+
+// IsExpired will return true if it is expired
+func (c Configs) IsExpired() bool {
+	return c.ExpiredAt.Before(time.Now())
+}
+
+// Config is a loaded config from ConfigFile
+var Config Configs
+
+// TokenExpire means expiration of the token
+const TokenExpire = time.Duration(20 * time.Hour)
+
+// GetToken returns access token for dozens
+func GetToken() (string, error) {
+	if Config.Token != "" && Config.IsExpired() {
+		return Config.Token, nil
+	}
+
+	if _, err := os.Stat(ConfigFile); os.IsNotExist(err) {
+		fmt.Fprintf(os.Stderr, "config file: `%s` does not exist. creating...\n", ConfigFile)
+		if err := createConfig(); err != nil {
+			return "", errors.Wrap(err, "error in createConfig")
+		}
+
+		return saveConfig()
+	}
+
+	if err := readConfig(); err != nil {
+		return "", errors.Wrap(err, "error in createConfig")
+	}
+
+	return saveConfig()
+}
+
+func createConfig() error {
+	if !Config.IsValid {
+		Config.AuthInfo = inputAuthInfo()
+	}
+
+	authorizeResp, err := GetAuthorize()
+	if err != nil {
+		return errors.Wrap(err, "errorin GetAuthorize")
+	}
+
+	Config.Token = authorizeResp.AuthToken
+	Config.ExpiredAt = time.Now().Add(TokenExpire)
+
+	return nil
+}
+
+func readConfig() error {
+	txt, err := ioutil.ReadFile(ConfigFile)
+	if err != nil {
+		return errors.Wrap(err, "error in ReadFile")
+	}
+
+	Config = Configs{}
+	if err := json.Unmarshal(txt, &Config); err != nil {
+		return errors.Wrap(err, "error in Unmarshal")
+	}
+
+	if Config.Token == "" || Config.IsExpired() {
+		createConfig()
+	}
+
+	return nil
+}
+
+func saveConfig() (string, error) {
+	configDir := filepath.Dir(ConfigFile)
+	if _, err := os.Stat(configDir); os.IsNotExist(err) {
+		if err := os.MkdirAll(configDir, 0777); err != nil {
+			return "", errors.Wrap(err, "error in MkDirAll")
+		}
+	}
+
+	if Config.Token != "" && !Config.IsExpired() {
+		Config.IsValid = true
+	}
+
+	json, err := json.Marshal(Config)
+	if err != nil {
+		return "", errors.Wrap(err, "error in Marshal")
+	}
+
+	if err := ioutil.WriteFile(ConfigFile, json, 0666); err != nil {
+		return "", errors.Wrap(err, "error in WriteFile")
+	}
+
+	return Config.Token, nil
+}
+
+func inputAuthInfo() AuthInfo {
+	key := prompter.Prompt("input API Key", "")
+	user := prompter.Prompt("input DozensID", "")
+	return AuthInfo{key, user}
 }

424517adf08ee0af0647f2954ce2600c3ae2875a
delphinus <delphinus@remora.cx>
2016-12-19 08:27:09 +0900

    Cut off logic for request

--- lib/dozens/request.go
+++ lib/dozens/request.go
@@ -10,35 +10,22 @@ import (
 
 // MakeGet returns request for dozens
 func MakeGet(token string, p endpoint.Endpoint) (*http.Request, error) {
-	req, err := http.NewRequest("GET", p.String(), nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "error in NewRequest")
-	}
-
-	if err := setHeader(token, req); err != nil {
-		return nil, errors.Wrap(err, "error in setHeader")
-	}
-
-	return req, nil
+	return makeRequest("GET", token, p, nil)
 }
 
 // MakePost returns request for dozens
 func MakePost(token string, p endpoint.Endpoint, body io.Reader) (*http.Request, error) {
-	req, err := http.NewRequest("POST", p.String(), body)
+	return makeRequest("POST", token, p, body)
+}
+
+func makeRequest(method, token string, p endpoint.Endpoint, body io.Reader) (*http.Request, error) {
+	req, err := http.NewRequest(method, p.String(), body)
 	if err != nil {
 		return nil, errors.Wrap(err, "error in NewRequest")
 	}
 
-	if err := setHeader(token, req); err != nil {
-		return nil, errors.Wrap(err, "error in setHeader")
-	}
-
-	return req, nil
-}
-
-func setHeader(token string, req *http.Request) error {
 	req.Header.Set("X-Auth-Token", token)
 	req.Header.Set("Content-Type", "application/json")
 
-	return nil
+	return req, nil
 }

257df11ad6b02b5c7444a7b69193e08362714440
delphinus <delphinus@remora.cx>
2016-12-19 18:34:09 +0900

    Deal with bug of dozens API

--- lib/dozens/record.go
+++ lib/dozens/record.go
@@ -23,7 +23,7 @@ type record struct {
 }
 
 func doRecordRequest(req *http.Request) (RecordResponse, error) {
-	recordResp := RecordResponse{}
+	recordResp := RecordResponse{[]record{}}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -31,15 +31,22 @@ func doRecordRequest(req *http.Request) (RecordResponse, error) {
 	}
 	defer resp.Body.Close()
 
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return recordResp, errors.Wrap(err, "error in ReadAll")
+	}
+
 	if resp.StatusCode != http.StatusOK {
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return recordResp, errors.Wrap(err, "error in ReadAll")
-		}
 		return recordResp, errors.Errorf("error body: %s", body)
 	}
 
-	if err := json.NewDecoder(resp.Body).Decode(&recordResp); err != nil {
+	// Dozens API has a bug. If records are nil, it responses `[]` instead of
+	// `{"record":[]}`.
+	if len(body) == 2 && string(body) == "[]" {
+		return recordResp, nil
+	}
+
+	if err := json.Unmarshal(body, &recordResp); err != nil {
 		return recordResp, errors.Wrap(err, "error in Decode")
 	}
 

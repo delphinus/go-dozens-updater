d40c364a3e053a4d5d38cb43663259ed869f4982
delphinus <delphinus@remora.cx>
2017-04-30 17:52:23 +0900

    Add tests for createConfig

--- config_test.go
+++ config_test.go
@@ -4,9 +4,14 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/delphinus/go-dozens"
+	"github.com/delphinus/go-dozens/endpoint"
+	"github.com/jarcoal/httpmock"
 )
 
 func TestSetupConfigHaveValidConfig(t *testing.T) {
@@ -39,6 +44,17 @@ func makeTmpConfig(ctx context.Context, txt string) (string, error) {
 	}
 
 	return tmp.Name(), nil
+}
+
+func makeMockDozens(ctx context.Context, method, url string, responder httpmock.Responder) {
+	httpmock.Activate()
+
+	go func() {
+		<-ctx.Done()
+		httpmock.DeactivateAndReset()
+	}()
+
+	httpmock.RegisterResponder(method, url, responder)
 }
 
 func TestSetupConfigCreateConfig(t *testing.T) {
@@ -85,5 +101,33 @@ func TestSetupConfigReadConfigValidly(t *testing.T) {
 
 	if err := SetupConfig(); err != nil {
 		t.Errorf("error occurred: %v", err)
+	}
+}
+
+func TestSetupConfigReadConfigCreateConfigAndFail(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	f, err := makeTmpConfig(ctx, fmt.Sprintf(`{
+		"key": "hoge",
+		"user": "fuga",
+		"token": "hoge",
+		"expiresAt": "%s"
+	}`, time.Now().Add(-time.Duration(1)*time.Minute).Format(time.RFC3339)))
+
+	if err != nil {
+		t.Errorf("error to create tmp config: %v", err)
+	}
+	original := ConfigFile
+	ConfigFile = f
+	defer func() {
+		cancel()
+		ConfigFile = original
+	}()
+
+	url := endpoint.Authorize().String()
+	responder, _ := httpmock.NewJsonResponder(http.StatusOK, &dozens.AuthorizeResponse{"hoge"})
+	makeMockDozens(ctx, "GET", url, responder)
+
+	if err := SetupConfig(); err != nil {
+		t.Errorf("error occuured: %v", err)
 	}
 }

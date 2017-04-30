f2664887703df5042d8e6d640cd224638ff9041c
delphinus <delphinus@remora.cx>
2017-04-30 17:18:02 +0900

    Add utility func for creating tmp files

--- config_test.go
+++ config_test.go
@@ -1,8 +1,8 @@
 package godo
 
 import (
+	"context"
 	"io/ioutil"
-	"path"
 	"strings"
 	"testing"
 	"time"
@@ -19,19 +19,45 @@ func TestSetupConfigHaveValidConfig(t *testing.T) {
 	}
 }
 
-func TestSetupConfigCreateConfig(t *testing.T) {
-	tmpDir, _ := ioutil.TempDir("", "")
+func makeTmpConfig(ctx context.Context, txt string) (string, error) {
+	tmp, err := ioutil.TempFile("", "")
+	if err != nil {
+		return "", err
+	}
+
 	originalConfigFile := ConfigFile
-	ConfigFile = path.Join(tmpDir, "config")
-	defer func() { ConfigFile = originalConfigFile }()
+	ConfigFile = tmp.Name()
+
+	go func() {
+		<-ctx.Done()
+		_ = tmp.Close()
+		ConfigFile = originalConfigFile
+	}()
+
+	if txt != "" {
+		_, err := tmp.WriteString(txt)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return tmp.Name(), nil
+}
+
+func TestSetupConfigCreateConfig(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	_, err := makeTmpConfig(ctx, "")
+	if err != nil {
+		t.Errorf("error to create tmp config: %v", err)
+	}
+	defer cancel()
 
 	Config = Configs{
 		IsValid: true,
 	}
 
-	err := SetupConfig()
 	expected := "error in createConfig"
-	if err == nil || strings.Index(err.Error(), expected) != 0 {
+	if err := SetupConfig(); err == nil || strings.Index(err.Error(), expected) != 0 {
 		t.Errorf("error differs: %v", err)
 	}
 }

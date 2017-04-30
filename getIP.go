416c45d802ff887f53fe20a29dd93a579c0e0aea
delphinus <delphinus@remora.cx>
2017-04-30 15:27:03 +0900

    Use dproxy to deal with JSON easily

--- getIP.go
+++ getIP.go
@@ -4,18 +4,12 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/koron/go-dproxy"
 	"github.com/pkg/errors"
 	"github.com/rdegges/go-ipify"
 )
 
 var ipv6URL = "https://jsonip.com"
-
-type ipv6Resp struct {
-	IP            string `json:"ip"`
-	About         string `json:"about"`
-	Pro           string `json:"Pro!"`
-	RejectFascism string `json:"reject-fascism"`
-}
 
 func getIP(ipv6 bool) (string, error) {
 	if !ipv6 {
@@ -28,10 +22,10 @@ func getIP(ipv6 bool) (string, error) {
 	}
 	defer func() { _ = resp.Body.Close() }()
 
-	var r ipv6Resp
-	if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+	var v interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&v); err != nil {
 		return "", errors.Wrap(err, "error in Decode")
 	}
 
-	return r.IP, nil
+	return dproxy.New(v).M("ip").String()
 }
